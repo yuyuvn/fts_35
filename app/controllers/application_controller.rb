0b8d4739c61ce84486b7a3aafe67b7c9fce76dda
PhuongND <phuongnd.bk@gmail.com>
2015-07-07 13:32:01 +0700

    Task #34166 User edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email,
-     :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit :name, :email,
+     :password, :password_confirmation}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit :name, :email,
+     :password, :password_confirmation, :current_password}
   end
 end
