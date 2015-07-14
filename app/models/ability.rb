15ca7a5e68dbd7fd43f4e774c59ed8964949f3ff
Clicia Scarlet <yuyuvn@mac.com>
2015-07-14 08:24:49 +0700

    Task/34755 Refactor models

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,11 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize user
     user ||= User.new
     if user.is_admin?
       can :manage, :all
-    end
-
-    if !user.is_guest?
+    elsif !user.is_guest?
       can :create, Exam
       can [:read, :update], Exam, user_id: user.id
     end
